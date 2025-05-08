46b8be6b8207b437a48bea64907351436cd7a2b1
aagii-art <altansvhaagii72@gmail.com>
2025-05-08 19:15:56 +0800

    djf

--- src/app/genreResults/page.tsx
+++ src/app/genreResults/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import GenreResultsPage from "./aa/page";
+import GenreResultsPage from "./psda";
 export default function Page() {
   return (
     <Suspense fallback={<div className="text-center mt-20">Уншиж байна...</div>}>
